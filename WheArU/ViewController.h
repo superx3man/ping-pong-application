c2c4922391d9137b6d9e1209c50dfcec472b4aeb
calvinng <cng@zerionsoftware.com>
2014-08-31 16:17:56 -0400

    Initial Commit

---
+++ WheArU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WheArU
+//
+//  Created by Calvin Ng on 8/31/14.
+//  Copyright (c) 2014 Hok Man Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WheArU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WheArU
+//
+//  Created by Calvin Ng on 8/31/14.
+//  Copyright (c) 2014 Hok Man Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
