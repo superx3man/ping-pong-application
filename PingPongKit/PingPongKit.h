07300d54f5090bd8c13da69016a6d9419b94f2f9
Calvin Ng <superx3man@gmail.com>
2016-04-03 09:29:14 -0400

    Initial Commit

---
+++ PingPongKit/PingPongKit.h
@@ -0,0 +1,19 @@
+//
+//  PingPongKit.h
+//  PingPongKit
+//
+//  Created by Calvin Ng on 2/10/15.
+//  Copyright (c) 2015 Hok Man Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PingPongKit.
+FOUNDATION_EXPORT double PingPongKitVersionNumber;
+
+//! Project version string for PingPongKit.
+FOUNDATION_EXPORT const unsigned char PingPongKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingPongKit/PublicHeader.h>
+
+
